56908a499616e5257bc65e7bdd3f7affd1fd7f58
M. Scott Ford <scott@corgibytes.com>
2012-02-15 11:51:15 -0500

    Generated rails project and added deface gem with dsl support.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DefaceDslSample::Application.config.secret_token = '0761dbb260762fc1201b36ed612116925bbc2075cd0a99bf80ffc7af592b9589dcd0c8e81e9cc60522074e0c88a7f2e38d8ba2492b268fe519038239d80c7d00'
